81955dc9faa113f7805122d6c21754c7797ef5eb
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-02-05 13:23:39 +0100

    Implementado el main creando el inventario y el jugador

--- src/Videojuego/MainVideojuego.java
+++ src/Videojuego/MainVideojuego.java
@@ -2,6 +2,11 @@ package Videojuego;
 
 public class MainVideojuego {
     public static void main(String[] args) {
-        
+        Objeto[] inventario = new Objeto[2];
+        inventario[0] = new Objeto("Poción A", 20);
+        inventario[1] = new Objeto("Poción B", 10);
+
+        Jugador jugador1 = new Jugador("Erik", 100, inventario);
+        jugador1.mostrarEstado();
     }
 }
