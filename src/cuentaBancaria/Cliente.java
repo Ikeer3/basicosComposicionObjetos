8537d0d609c012efaba0e12aa7ba73be7c4ab5a1
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-02-09 18:00:10 +0100

    Creada clase Cliente con su constructor

---
+++ src/cuentaBancaria/Cliente.java
@@ -0,0 +1,12 @@
+package cuentaBancaria;
+
+public class Cliente {
+
+    private String nombre;
+    private int DNI;
+
+    public Cliente(String nombre, int DNI) {
+        this.nombre = nombre;
+        this.DNI = DNI;
+    }
+}
