e9440e16dc95a53afa6a27c0a319d8c50e0f0bef
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-03-10 10:13:47 +0100

    Creado el main con el array y se implementa el metodo con la información

--- src/Equipo/MainEquipo.java
+++ src/Equipo/MainEquipo.java
@@ -3,6 +3,15 @@ package Equipo;
 public class MainEquipo {
     public static void main(String[] args) {
 
+        Jugador[] plantilla = new Jugador[5];
+        plantilla[0] = new Jugador("Carlos", "portero");
+        plantilla[1] = new Jugador("Iván", "defensa");
+        plantilla[2] = new Jugador("Iker", "delantero");
+        plantilla[3] = new Jugador("Daniel", "mediocentro");
+        plantilla[4] = new Jugador("Isabella", "lateral");
 
+        Equipo equipo1 = new Equipo("Liceo FC", plantilla);
+
+        equipo1.mostrarInfo();
     }
 }
