efeeebcf6e3db70a5ca9afafff74bec2db91cfc2
Iker Santos Insua <ikersantosinsua@gmail.com>
2026-03-12 12:07:54 +0100

    Implementado el main con la lista de productos

--- src/Pedidos/MainPedidos.java
+++ src/Pedidos/MainPedidos.java
@@ -3,6 +3,14 @@ package Pedidos;
 public class MainPedidos {
     public static void main(String[] args) {
 
-        
+        Producto[] listaCompra = new Producto[4];
+        listaCompra[0] = new Producto("Pepinillos", 3.2);
+        listaCompra[1] = new Producto("Pizza", 5.6);
+        listaCompra[2] = new Producto("Leche", 7.4);
+        listaCompra[3] = new Producto("Rape", 17.8);
+
+        Pedido pedido1 = new Pedido(1, listaCompra);
+
+        pedido1.calularPrecioTotal();
     }
 }
